74ef5aa885f557efa10e54bc20e33f8c260d7bcb
IgnaciaJ <ignacia.joustra@uc.cl>
2023-05-28 18:58:28 -0400

    feature:setup-with-docker-compose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HOLI!! MY-APP.
+         Ojalá
         </p>
         <a
           className="App-link"
